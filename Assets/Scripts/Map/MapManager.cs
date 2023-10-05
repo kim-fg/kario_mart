12931d2f706c44c6994b720388dbaa29dc5b8ffb
Kim <joakim.linna@edu.futuregames.nu>
2023-10-05 13:57:53 +0200

    added pathbuilder, let mapmanager hold mapdata

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,25 +1,52 @@
+using System;
+using System.IO;
+using KarioMart.Gamemodes.TimeTrial;
+using KarioMart.Util;
 using UnityEngine;
 
 namespace KarioMart.Map
 {
     public class MapManager : MonoBehaviour
     {
+        [SerializeField] private MapData mapData;
         [SerializeField] private Collider2D[] checkpoints;
         [SerializeField] private Transform[] startGridPositions;
-        
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
 
         public Collider2D[] Checkpoints => checkpoints;
         public Transform[] StartGridPositions => startGridPositions;
+
+        private TrackLeaderboard _trackLeaderboard;
+
+        private void Start()
+        {
+            LoadTrackLeaderboard();
+        }
+
+        public TrackLeaderboard TrackLeaderboard
+        {
+            get
+            {
+                if(_trackLeaderboard == null)
+                    LoadTrackLeaderboard();
+                return _trackLeaderboard;
+            }
+
+            set
+            {
+                _trackLeaderboard = value;
+                SaveTrackLeaderboard();
+            }
+        }
+
+        private string TrackLeaderboardPath => PathBuilder.BuildJson(true, "leaderboards", mapData.DisplayName);
+
+        private void LoadTrackLeaderboard()
+        {
+            print(TrackLeaderboardPath);
+        }
+        private void SaveTrackLeaderboard()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ Assets/Scripts/Util/PathBuilder.cs
@@ -0,0 +1,31 @@
+using System.Text;
+using UnityEngine;
+
+namespace KarioMart.Util
+{
+    public static class PathBuilder
+    {
+        public static string Build(bool fromPersistentDataPath, params object[] pathSegments)
+        {
+            var stringBuilder = new StringBuilder();
+            if (fromPersistentDataPath)
+                stringBuilder.Append(Application.persistentDataPath);
+            
+            for (int i = 0; i < pathSegments.Length; i++)
+            {
+                stringBuilder.Append(pathSegments[i]);
+                if (i < pathSegments.Length - 1)
+                    stringBuilder.Append('/');
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string BuildJson(bool fromPersistentDataPath, params object[] pathSegments)
+        {
+            var path = Build(fromPersistentDataPath, pathSegments);
+            path += ".json";
+            return path;
+        }
+    }
+}
